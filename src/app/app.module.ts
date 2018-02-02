280c207196753dae4126fe310c73773635eb51d5
Andrei Pit <charmpitz@gmail.com>
2018-02-02 02:32:18 +0200

    added menu component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { HomePage } from './page/home/home.page';
 import { ROUTES } from './routing'
 import { environment } from '../environments/environment';
+
+// Pages
+import { HomePage } from './page/home/home.page';
+
+// Components
+import { MenuComponent } from './component/menu/menu.component';
 
 @NgModule({
     // Own module components
     declarations: [
         AppComponent,
-        HomePage
+        HomePage,
+        MenuComponent
     ],
 
     // Other Modules that contain components
