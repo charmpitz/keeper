35d65dd03162de0a0b00cd1556f06b21aa14d649
Andrei Pit <charmpitz@gmail.com>
2018-02-02 02:04:53 +0200

    configured angular and added router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-
+import { HomePage } from './page/home/home.page';
+import { ROUTES } from './routing'
+import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    // Own module components
+    declarations: [
+        AppComponent,
+        HomePage
+    ],
+
+    // Other Modules that contain components
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(ROUTES, {
+            useHash: false ,
+            enableTracing: environment.router.enableTracing
+        })
+    ],
+
+    // Singleton services
+    providers: [],
+
+    bootstrap: [
+        AppComponent
+    ]
 })
+
 export class AppModule { }
---
+++ src/app/routing.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { HomePage } from './page/home/home.page'
+
+export const ROUTES: Routes = [
+    {
+        path: '',
+        component: HomePage
+    }
+];
